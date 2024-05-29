330aea17beefc6ffc96e0f5708eb4211dfbdf16a
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2024-05-29 16:14:11 +0700

    feat: signup only

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAbYYMEwMLzqtYyHv1cuegqRQj7S2EckfE",
@@ -11,8 +12,9 @@ const firebaseConfig = {
     appId: "1:239458108126:web:1d99bc42514fde000eb84f"
   };
   
-  const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-  const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, db };
+export { app, db, auth };
