94764f387b9b6e27253387725f12220d323ddbc3
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2024-05-29 20:58:02 +0700

    update uiux

--- components/RouteMap.jsx
+++ components/RouteMap.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
-import MapView, { PROVIDER_GOOGLE, PROVIDER_DEFAULT, Marker } from "react-native-maps";
+import MapView, {
+  PROVIDER_GOOGLE,
+  PROVIDER_DEFAULT,
+  Marker,
+} from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import * as Location from "expo-location";
 import originPoint from "../assets/images/originPoint.png";
 import destinationPoint from "../assets/images/destinationPoint.png";
-import { Image, Platform } from "react-native";
+import { Platform, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const GOOGLE_MAPS_APIKEY = "AIzaSyBmW2jQNUVsWY6etVO-UTwh4kBUxMi-e2w";
 
@@ -32,6 +37,17 @@ const RouteMap = ({ origin, destination }) => {
     return null;
   }
 
+  const centerMapOnUser = async () => {
+    // let currentLocation = await Location.getCurrentPositionAsync({});
+    // setLocation(currentLocation);
+    mapRef.current.animateToRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0222,
+      longitudeDelta: 0.0121,
+    });
+  };
+
   const originLoc = originFinal
     ? {
         latitude: originFinal.details.geometry.location.lat,
@@ -50,37 +66,52 @@ const RouteMap = ({ origin, destination }) => {
     : null;
 
   return (
-    <MapView
-      style={{ width: "100%", height: "100%" }}
-      provider={Platform.OS === 'android' ? PROVIDER_GOOGLE : PROVIDER_DEFAULT}
-      showsUserLocation={true}
-      initialRegion={{
-        latitude: originLoc.latitude,
-        longitude: originLoc.longitude,
-        latitudeDelta: 0.0222,
-        longitudeDelta: 0.0121,
-      }}
-    >
-      {destinationLoc && originFinal && (
-        <MapViewDirections
-          origin={originLoc}
-          destination={destinationLoc}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={5}
-          strokeColor="blue"
-        />
-      )}
-      {originFinal && (
-        <Marker coordinate={originLoc} title={"Origin"} icon={originPoint}>
-          {/* <Image source={originPoint} className="w-5 h-5" /> */}
-        </Marker>
-      )}
-      {destinationLoc && (
-        <Marker coordinate={destinationLoc} title={"Destination"} icon={destinationPoint}>
-          {/* <Image source={destinationPoint} className="w-5 h-5" /> */}
-        </Marker>
-      )}
-    </MapView>
+    <View>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        provider={
+          Platform.OS === "android" ? PROVIDER_GOOGLE : PROVIDER_DEFAULT
+        }
+        showsUserLocation={true}
+        showsMyLocationButton={false}
+        initialRegion={{
+          latitude: originLoc.latitude,
+          longitude: originLoc.longitude,
+          latitudeDelta: 0.0222,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        {destinationLoc && originFinal && (
+          <MapViewDirections
+            origin={originLoc}
+            destination={destinationLoc}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={5}
+            strokeColor="blue"
+          />
+        )}
+        {originFinal && (
+          <Marker coordinate={originLoc} title={"Origin"} icon={originPoint}>
+            {/* <Image source={originPoint} className="w-5 h-5" /> */}
+          </Marker>
+        )}
+        {destinationLoc && (
+          <Marker
+            coordinate={destinationLoc}
+            title={"Destination"}
+            icon={destinationPoint}
+          >
+            {/* <Image source={destinationPoint} className="w-5 h-5" /> */}
+          </Marker>
+        )}
+      </MapView>
+      <TouchableOpacity
+        className="absolute items-center justify-center w-10 h-10 bg-blue-500 bottom-5 right-5 rounded-xl"
+        onPress={centerMapOnUser}
+      >
+        <Ionicons name="locate" size={28} color="white" />
+      </TouchableOpacity>
+    </View>
   );
 };
 
